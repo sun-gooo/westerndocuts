a71d23ecc58d6665d74e23c753fa1b0a35d684d0
damin <gritdz555@gmail.com>
2024-11-08 22:52:26 +0900

    Update connect.ts

--- hacks/src/db/connect.ts
+++ hacks/src/db/connect.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
-
+//パスワード等は個人情報なので隠します。必要でしたら教えてください
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "Tk?rz139",
-  database: process.env.DB_NAME || "hacks",
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASS ,
+  database: process.env.DB_NAME ,
 });
